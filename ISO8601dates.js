8c89d92f6b2c2ce9a2b8134fc4c8d17e6db9e26e
antonegas <32789589+antonegas@users.noreply.github.com>
2021-12-30 13:51:35 +0100

    Added Date.prototype.getISOWeek()

--- ISO8601dates.js
+++ ISO8601dates.js
@@ -1,4 +1,16 @@
-// Current day of the week with monday as zero
+// Get current day of the week with monday as zero.
 Date.prototype.getISODay = function () {
-  return (this.getDay() || 7) - 1;
+  return (this.getUTCDay() || 7) - 1;
+};
+
+// Get current number of the week.
+// As per RobG (2011-05-24) answer on the StackOverflow question:
+// https://stackoverflow.com/questions/6117814/get-week-of-year-in-javascript-like-in-php
+// Modified to use correction from Jacob Lauritzen (2016-01-04).
+Date.prototype.getISOWeek = function () {
+  var d = new Date(Date.UTC(this.getFullYear(), this.getMonth());
+  d.setHours(0, 0, 0, 0);
+  d.setUTCDate(d.getUTCDate() + 4 - d.getISODay());
+  var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+  return Math.ceil((((d - yearStart) / 86400000) + 1)/7);
 };
