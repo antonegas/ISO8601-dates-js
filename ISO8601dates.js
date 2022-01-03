92a31a6ef792c0061e0ee5e5fe23b453c1b6c223
antonegas <32789589+antonegas@users.noreply.github.com>
2022-01-03 17:41:53 +0100

    Added function for getting number of days between two dates

--- ISO8601dates.js
+++ ISO8601dates.js
@@ -13,3 +13,13 @@ Date.prototype.getISOWeek = function () {
   var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
   return Math.ceil((((d - yearStart) / 86400000) + 1)/7)
 };
+
+// Get number of days between two dates.
+// A version of Miles (2009-02-12) datediff function in the answer on the
+// StackOverflow question:
+// https://stackoverflow.com/questions/542938/how-to-calculate-number-of-days-between-two-dates
+// With the use of Math.trunc instead of round as per Jin Wang's (2016-07-06)
+// comment.
+Date.daysBetween = function (day1, day2) {
+  return Math.trunc((d1 - d2)/86400000)
+};
